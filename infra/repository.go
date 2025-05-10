c72b78b5e4bddfe9cd04962e5976e5a4f5585784
Italo Alves <italorenan225@gmail.com>
2025-05-10 19:42:09 -0300

    feat: add prof

--- infra/repository.go
+++ infra/repository.go
@@ -2,7 +2,9 @@ package infra
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/lib/pq"
 )
@@ -26,21 +28,26 @@ func (r *repository) SaveMultiplesMovies(movies []*Movie) error {
 	}
 	defer tx.Rollback()
 
-	stmt, err := tx.Prepare("INSERT INTO movies (id, title, year, genres) VALUES ($1, $2, $3, $4)")
-	if err != nil {
-		log.Fatalf("erro no filme: %s", "movie.Title")
+	var (
+		valueStrings []string
+		valueArgs    []any
+	)
+
+	for i, movie := range movies {
+		// Cada grupo de valores precisa de placeholders únicos: ($1, $2, $3, $4), ...
+		n := i * 4
+		valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d, $%d)", n+1, n+2, n+3, n+4))
+		valueArgs = append(valueArgs, movie.Id, movie.Title, movie.Year, pq.Array(movie.Genres))
+	}
+
+	stmt := fmt.Sprintf("INSERT INTO movies (id, title, year, genres) VALUES %s", strings.Join(valueStrings, ","))
+
+	if _, err := tx.Exec(stmt, valueArgs...); err != nil {
+		log.Printf("erro no bulk insert: %s", err)
 		return err
 	}
-	defer stmt.Close()
 
-	for _, movie := range movies {
-		if _, err = stmt.Exec(movie.Id, movie.Title, movie.Year, pq.Array(movie.Genres)); err != nil {
-			log.Fatalf("erro no filme: %s", movie.Title)
-			return err
-		}
-	}
-
-	if err = tx.Commit(); err != nil {
+	if err := tx.Commit(); err != nil {
 		return err
 	}
 
--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
+
+	"os"
+	"runtime/pprof"
 
 	"github.com/RenanAlmeida225/concurrent-movie-uploader/infra"
 	"github.com/RenanAlmeida225/concurrent-movie-uploader/service"
 )
 
 func main() {
-	start := time.Now()
 	db := infra.New()
 	r := infra.NewRepo(db)
 	s := service.New(r)
 
-	movies := make(chan []*infra.Movie, 2)
+	movies := make(chan []*infra.Movie, 20)
+
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal("could not create CPU profile: ", err)
+	}
+	defer f.Close()
+
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal("could not start CPU profile: ", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	start := time.Now()
 
 	go s.ReadCSV("movies.csv", movies)
 
